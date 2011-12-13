ab21e67df9c4e04bb25cf4f27079be82317fbecd
Nicholas Bishop <nicholasbishop@gmail.com>
2011-12-13 05:17:44 -0500

    Change main to output to sql db rather than stdout

--- main.cc
+++ main.cc
@@ -8,8 +8,16 @@
 using namespace Odis;
 
 void init_project_db(Sqlite &db) {
+	/* create file table */
 	db.exec("CREATE TABLE IF NOT EXISTS file "
 			"(filepath TEXT, data BLOB)");
+
+	/* create part table */
+	db.exec("CREATE TABLE IF NOT EXISTS part "
+			"(part_type TEXT, file_id INTEGER, "
+			"name TEXT, "
+			"file_offset INTEGER, file_size INTEGER, "
+			"virtual_addr INTEGER, virtual_size INTEGER)");
 }
 
 char *read_file(const char *filepath, size_t *file_length) {
@@ -48,17 +56,6 @@ void project_db_add_file(Sqlite &db, const char *filepath) {
 }
 
 int main(int argc, char **argv) {
-	/*try {
-	  Loader *loader = Odis::elf_loader_create_from_file(argv[1]);
-	  LoaderOutput *output = new LoaderOutputSQL();
-		
-	  while(loader->read(output));
-	  delete loader;
-	  }
-	  catch(std::exception *e) {
-	  std::cerr << e->what() << std::endl;
-	  }*/
-
 	if(argc != 2)
 		throw std::exception();
 
@@ -71,14 +68,19 @@ int main(int argc, char **argv) {
 	size_t file_length;
 	char *data = read_file(argv[1], &file_length);
 	FileInfo *info = file_info_elf(data, file_length);
+	Sqlite::Stmt stmt(db, "INSERT INTO part VALUES(?,?,?,?,?,?,?)");
+	
 	for(auto parts : info->parts()) {
-		std::cout << parts.first << ": " << std::endl;
-
 		for(auto p : parts.second) {
-			std::cout << "  " << p.name << std::hex
-					  << ", file(0x" << p.file_offset << ", 0x" << p.file_size
-					  << "), virt(0x" << p.virtual_addr << ", 0x" << p.virtual_size
-					  << ")" << std::endl;
+			stmt.bind_text_with_copy(1, parts.first.c_str());
+			stmt.bind_int(2, 1); /* TODO */
+			stmt.bind_text_with_copy(3, p.name.c_str());
+			stmt.bind_int(4, p.file_offset);
+			stmt.bind_int(5, p.file_size);
+			stmt.bind_int(6, p.virtual_addr);
+			stmt.bind_int(7, p.virtual_size);
+			db.step(stmt);
+			stmt.reset();
 		}
 	}
 	delete info;
